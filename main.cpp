b6edf89159f55cdd2660e6ea2fe17a04eda20f7d
xJAPster <jona4385@gmail.com>
2024-09-12 15:00:27 +0530

    pre mp implementation

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ int player1Score=0, player2Score=0, CPUScore=0;
 Sound paddle_hit, bounds_hit, point_scored; 
 Image menuasset, endasset;
 Texture2D menuscr, endscr;
+bool menuexit=false;
+bool gameover=false;
 
 class Ball{
 public:
@@ -31,7 +33,7 @@ public:
     }
 };
 
-class PlayerPaddle{
+class Player1Paddle{
 public:
     float x, y;
     int speed;
@@ -46,6 +48,7 @@ public:
         if(IsKeyDown(KEY_S))y+=speed;
         Limit();
     }
+
 protected:
     void Limit(){
         //out of bounds
@@ -54,7 +57,16 @@ protected:
     }
 };
 
-class CPU : public PlayerPaddle{
+class Player2Paddle : public Player1Paddle{
+public:
+    void Update(){
+        if(IsKeyDown(KEY_UP))y-=speed;
+        if(IsKeyDown(KEY_DOWN))y+=speed;
+        Limit();
+    }
+};
+
+class CPU : public Player1Paddle{
 public:
     //overriding Update() method
     void Update(int ball_y){
@@ -66,16 +78,20 @@ public:
 
 //declaration of objects
 Ball ball;
-PlayerPaddle player1;
+Player1Paddle player1;
+Player2Paddle player2;
 CPU cpu_paddle;
+
+void menuScreen();
+void gameoverScreen();
+void singleplayer();
+void multiplayer();
 
 int main (){
     //setup
     const int width=1280;
     const int height=800;
-    bool menuexit=false;
-    bool gameover=false;
-
+    
     InitAudioDevice(); // loading audio sound fx
     paddle_hit = LoadSound("assets/paddle_hit.mp3");
     bounds_hit = LoadSound("assets/boundary_hit.mp3");
@@ -97,6 +113,12 @@ int main (){
     player1.height=120;
     player1.speed=7;
 
+    player2.x=width-35;
+    player2.y=height/2-60;
+    player2.width=25;
+    player2.height=120;
+    player2.speed=7;
+
     cpu_paddle.x=width-35;
     cpu_paddle.y=height/2-60;
     cpu_paddle.width=25;
@@ -105,85 +127,19 @@ int main (){
     
     //game loop start
     while(!WindowShouldClose()){
-
         //MAIN MENU SCREEN ----------------------------------------------
-        while(!WindowShouldClose() && menuexit==false){
+        menuScreen();
 
-            if(IsKeyPressed(KEY_ENTER)){
-                menuexit=true;
-                break;
-            }
+        //GAME OVER SCREEN ----------------------------------------------
+        gameoverScreen();
+        if(gameover) break;
 
-            BeginDrawing();
-
-            ClearBackground(BLACK);
-            menuasset=LoadImage("assets/MENUASSET2.png");
-            menuscr=LoadTextureFromImage(menuasset);
-
-            DrawTexture(menuscr,0,0,WHITE);
-
-            EndDrawing();
-
-            if(WindowShouldClose())gameover=true;
-
-        }
-
-        //GAMEPLAY ----------------------------------------------
-        BeginDrawing();
-        
-        //drawing the ball
-        ball.Update();
-        ball.Draw();
-        
-        //drawing the paddles
-        player1.Draw();
-        player1.Update();
-
-        cpu_paddle.Draw();
-        cpu_paddle.Update(ball.y);
-
-        //collision detection
-        if(CheckCollisionCircleRec(Vector2{ball.x,ball.y}, 15, Rectangle{player1.x, player1.y,player1.width,player1.height})){
-            ball.speedx*=-1;
-            PlaySound(paddle_hit);
-        }
-        if(CheckCollisionCircleRec(Vector2{ball.x,ball.y}, 15, Rectangle{cpu_paddle.x, cpu_paddle.y,cpu_paddle.width,cpu_paddle.height})){
-            ball.speedx*=-1;
-            PlaySound(paddle_hit);
-        }
-        
-        //scoreboard
-        DrawText(TextFormat("YOU: %i", player1Score), width/4-20, 20, 30, WHITE);
-        DrawText(TextFormat("CPU: %i", CPUScore), 3*width/4-20, 20, 30, WHITE);
-
-        DrawLine(width/2,height,width/2,0,WHITE);
-        ClearBackground(BLACK);
-        HideCursor();
+        //GAMEPLAY ------------------------------------------------------
+        singleplayer();
 
         //game over condition 
-        if(player1Score==5||CPUScore==5) gameover=true;
+        if(player1Score==5||player2Score==5||CPUScore==5) gameover=true;
         EndDrawing();
-
-        //GAME OVER SCREEN -----------------------------------------
-        while(!WindowShouldClose() && !IsKeyPressed(KEY_ENTER) && gameover==true){
-            BeginDrawing();
-
-            endasset = LoadImage("assets/ENDSCREEN_ASSET1.png");
-            endscr=LoadTextureFromImage(endasset);
-            DrawTexture(endscr,0,0,WHITE);
-
-            EndDrawing();
-
-            if(IsKeyPressed(KEY_R)){
-                gameover=false;
-                player1Score=0;
-                player2Score=0;
-                CPUScore=0;
-                break;
-            }
-        }
-
-        if(gameover==true) break;
     }
     
     //handling opened resources
@@ -217,9 +173,116 @@ void Ball::Update(){
         ResetBall();
     }
     if(x-radius <=0){
-        //player2Score++;
+        player2Score++;
         PlaySound(point_scored);
         CPUScore++;
         ResetBall();
     }
+}
+
+void menuScreen(){
+    while(!WindowShouldClose() && menuexit==false){
+        if(IsKeyPressed(KEY_ENTER)){
+            menuexit=true;
+            return;
+        }
+
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+        menuasset=LoadImage("assets/MENUASSET2.png");
+        menuscr=LoadTextureFromImage(menuasset);
+
+        DrawTexture(menuscr,0,0,WHITE);
+
+        EndDrawing();
+
+        if(WindowShouldClose())gameover=true;
+    }
+}
+
+void gameoverScreen(){
+    while(!WindowShouldClose() && !IsKeyPressed(KEY_ENTER) && gameover==true){
+        BeginDrawing();
+
+        endasset = LoadImage("assets/ENDSCREEN_ASSET1.png");
+        endscr=LoadTextureFromImage(endasset);
+        DrawTexture(endscr,0,0,WHITE);
+
+        EndDrawing();
+
+        if(IsKeyPressed(KEY_R)){
+            gameover=false;
+            player1Score=0;
+            player2Score=0;
+            CPUScore=0;
+            return;
+        }
+    }
+}
+
+void singleplayer(){
+    BeginDrawing();
+    
+    //drawing the ball
+    ball.Update();
+    ball.Draw();
+    
+    //drawing the paddles
+    player1.Draw();
+    player1.Update();
+
+    cpu_paddle.Draw();
+    cpu_paddle.Update(ball.y);
+
+    //collision detection
+    if(CheckCollisionCircleRec(Vector2{ball.x,ball.y}, 15, Rectangle{player1.x, player1.y,player1.width,player1.height})){
+        ball.speedx*=-1;
+        PlaySound(paddle_hit);
+    }
+    if(CheckCollisionCircleRec(Vector2{ball.x,ball.y}, 15, Rectangle{cpu_paddle.x, cpu_paddle.y,cpu_paddle.width,cpu_paddle.height})){
+        ball.speedx*=-1;
+        PlaySound(paddle_hit);
+    }
+    
+    //scoreboard
+    DrawText(TextFormat("YOU: %i", player1Score), (GetScreenWidth())/4-20, 20, 30, WHITE);
+    DrawText(TextFormat("CPU: %i", CPUScore), 3*(GetScreenWidth())/4-20, 20, 30, WHITE);
+
+    DrawLine((GetScreenWidth())/2, GetScreenHeight(), (GetScreenWidth())/2, 0, WHITE);
+    ClearBackground(BLACK);
+    HideCursor();
+}
+
+void multiplayer(){ //to be implemented
+    BeginDrawing();
+    
+    //drawing the ball
+    ball.Update();
+    ball.Draw();
+    
+    //drawing the paddles
+    player1.Draw();
+    player1.Update();
+
+    player2.Draw();
+    player2.Update();
+
+    //collision detection
+    if(CheckCollisionCircleRec(Vector2{ball.x,ball.y}, 15, Rectangle{player1.x, player1.y,player1.width,player1.height})){
+        ball.speedx*=-1;
+        PlaySound(paddle_hit);
+    }
+    if(CheckCollisionCircleRec(Vector2{ball.x,ball.y}, 15, Rectangle{player2.x, player2.y, player2.width, player2.height})){
+        ball.speedx*=-1;
+        PlaySound(paddle_hit);
+    }
+    
+    //scoreboard
+    DrawText(TextFormat("YOU: %i", player1Score), (GetScreenWidth())/4-20, 20, 30, WHITE);
+    DrawText(TextFormat("CPU: %i", player2Score), 3*(GetScreenWidth())/4-20, 20, 30, WHITE);
+
+    DrawLine((GetScreenWidth())/2, GetScreenHeight(), (GetScreenWidth())/2, 0, WHITE);
+    ClearBackground(BLACK);
+    HideCursor();
 }
